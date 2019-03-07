b35f5aa4d8fd4917709c11d8049cb1c1651e2610
sgzhe <6485785@qq.com>
2019-03-07 10:31:36 +0800

    modify permission

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -1,7 +1,26 @@
 class Permission
   include ModelBase
 
+  field :operations, type: Array, default: []
+
   belongs_to :aro, class_name: 'Aro'
   belongs_to :aco, polymorphic: true
+
+  def permit(operation)
+    operations.any? { |op| op.to_s == operation.to_s }
+  end
+
+  class << self
+    def permit(aro_id, aco_id, operation)
+      @permissions ||= all.to_a
+      perm = @permissions.detect { |pe| pe.aro_id.to_s == aro_id.to_s && pe.aco_id.to_s == aco_id.to_s }
+      perm &. permit(operation)
+    end
+
+    def load
+      @permissions = all.to_a
+    end
+  end
+
 
 end

ef070b8f99572af9804b3a4f1f5c2c7711e199bb
songgz <sgzhe@163.com>
2019-03-23 05:48:42 +0800

    modify attendance

--- app/models/tracker.rb
+++ app/models/tracker.rb
@@ -10,23 +10,19 @@ class Tracker
 
   validates :user_id, uniqueness: {scope: :month, message: "should happen once per month"}
 
-  def self.pass(user, gateway, direction, pass_time = Time.now)
+  def self.pass(user, access, direction, pass_time = Time.now)
     t = Tracker.find_or_initialize_by(user: user, month: pass_time.at_beginning_of_month)
-    t.traces.build(pass_time: pass_time, gateway: gateway, direction: direction)
-    t.checking_attendance(pass_time, direction) if gateway.parent_id == user.room.parent_id
+    t.traces.build(pass_time: pass_time, access: access, direction: direction)
+    t.checking(pass_time, direction) if access.parent_id == user.room.parent_id
     t.save
   end
 
-  def checking_attendance(pass_time, direction)
-    at = attendances.find_or_initialize_by(day: pass_time.at_beginning_of_day)
-    if direction.to_sym == :out
-      at.status = :not_back
-    else
-      if pass_time.hour > 22
-        at.status = :back_late
-      else
-        at.status = :back
-      end
+  def checking(pass_time, direction)
+    h = Time.new(pass_time.year, pass_time.month, pass_time.day, 10, 30)
+    if (pass_time > h) && (direction == :in)
+      comer = Latecomer.find_or_initialize_by(user_id: user_id, day: pass_time.to_date, updated_at: pass_time)
+      comer.status = :back_late
+      comer.save
     end
   end
 

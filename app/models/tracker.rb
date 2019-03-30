825c2bccd84bd095518172df71d27c0f2471cdef
songgz <sgzhe@163.com>
2019-03-30 22:19:29 +0800

    modify tracker

--- app/models/tracker.rb
+++ app/models/tracker.rb
@@ -21,7 +21,7 @@ class Tracker
   def self.pass(user, access, direction, pass_time = DateTime.now)
     t = Tracker.find_or_initialize_by(user: user, month: pass_time.at_beginning_of_month.to_date)
     last = ((t.pass_time_at_last || DateTime.now).to_date + access.closing_at.minutes).to_datetime
-    timeout = (pass_time - last).to_f
+    timeout = ((pass_time - last).to_f * 24).to_i
     if direction == :in
       state = :back
       state = :back_late if timeout.positive?
@@ -33,14 +33,14 @@ class Tracker
     t.status_at_last = state
     t.pass_time_at_last = pass_time
     t.direction_at_last = direction
-    t.overtime = timeout.to_i if timeout.positive?
+    t.overtime = timeout.positive? ? timeout : 0
     t.traces.build(pass_time: pass_time, access: access, direction: direction)
     t.save
 
     if state.to_sym == :back_late
       comer = Latecomer.find_or_initialize_by(user: user, day: pass_time.to_date)
       comer.status = state
-      comer.overtime = (timeout * 24).to_i
+      comer.overtime = timeout
       comer.pass_time = pass_time
       comer.save
     end

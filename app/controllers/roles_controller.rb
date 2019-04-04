6c3c06ba3f59adb6304aba4cd7f347d4bdb8de10
songgz <sgzhe@163.com>
2019-04-04 16:50:53 +0800

    Update roles_controller.rb

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -48,6 +48,6 @@ class RolesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def role_params
-      params.require(:role).permit(:title)
+      params.require(:role).permit!
     end
 end

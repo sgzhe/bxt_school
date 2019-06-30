cbd834f5dc7d47554fd8cb5ca200ab53675e1e07
songgz <sgzhe@163.com>
2019-07-01 05:49:02 +0800

    modify refresh

--- app/controllers/incomings_controller.rb
+++ app/controllers/incomings_controller.rb
@@ -10,7 +10,9 @@ class IncomingsController < ApplicationController
         org_ids: org_id,
         status_at_last: params[:status],
         :overtime_at_last.gte => params[:overtime],
-        :pass_time_at_last.lte => params[:reside] && params[:reside].to_i.days.ago
+        :pass_time_at_last.lte => params[:reside] && params[:reside].to_i.days.ago,
+        :pass_time_at_last.gte => params[:start_at],
+        :pass_time_at_last.lte => params[:end_at]
     }.delete_if { |key, value| value.blank? }
     query = []
     unless params[:key].blank?
@@ -21,7 +23,7 @@ class IncomingsController < ApplicationController
     match = {facility_ids: facility_id,
              org_ids: org_id}.delete_if { |key, value| value.blank? }
     @status_stats = Student.status_stats(match)
-    @users = paginate(Student.includes(:dept, :dorm).where(opts).or(query).order_by(pass_time_at_last: -1).all)
+    @users = paginate(Student.includes(:dept, :dorm).where(opts).or(query).order_by(pass_time_at_last: -1))
 
   end
 

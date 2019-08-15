865d6f15a40ad9eea9d3e7320e9a7f0701a08981
songgz <sgzhe@163.com>
2019-08-15 20:59:17 +0800

    modify face

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -6,7 +6,7 @@ class Facility
   field :full_title, default: ''
   field :mark
 
-  default_scope -> { order_by(id: -1) }
+  default_scope -> { order_by(seq: -1, id: -1) }
 
   belongs_to :owner, foreign_key: :owner_id, class_name: 'User', required: false
   has_many :users, class_name: 'User', foreign_key: :facility_id, inverse_of: :facility, validate: false, dependent: :restrict_with_exception

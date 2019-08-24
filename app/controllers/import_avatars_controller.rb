d92ba729e2669fe5026e5833541effe466c9012c
songgz <sgzhe@163.com>
2019-08-24 08:16:59 +0800

    import student

--- app/controllers/import_avatars_controller.rb
+++ app/controllers/import_avatars_controller.rb
@@ -15,10 +15,22 @@ class ImportAvatarsController < ApplicationController
   # POST /import_avatars
   # POST /import_avatars.json
   def create
-    p request.form_data?
-    p request.body_stream
-    p params[:avatar]
-    request
+    #p request.form_data?
+    #p YAML.load(request.body_stream)
+    #p request.body_stream.size
+    # File.open('d:/data.jpg', 'w') do |f|
+    #   f.puts(request.body_stream.read)
+    # end
+
+    p import_avatar_params
+    # uploaded_io = import_avatar_params
+    # File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
+    #   file.write(uploaded_io.read)
+    # end
+    #
+    # s = Student.find_by(sno: 'S318107024')
+    # s.avatar = import_avatar_params
+    # s.save
     @import_avatar = ImportAvatar.new(import_avatar_params)
 
     if @import_avatar.save
@@ -52,6 +64,6 @@ class ImportAvatarsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def import_avatar_params
-      params.fetch(:import_avatar, {}).permit!
+      params.fetch(:avatar, {})
     end
 end
--- app/models/import_avatar.rb
+++ app/models/import_avatar.rb
@@ -2,11 +2,15 @@ class ImportAvatar
   include ActiveModel::Model
   attr_reader :data
 
-  def initialize(opts = {})
-
+  def initialize(import_avatar)
+    @user = Student.find_by(sno: 'S318107024')
+    @user.avatar = import_avatar
+    @data = { original_filename: import_avatar.original_filename }
   end
 
   def save
-    true
+    @user.save
+    @data[:avatar_url] = @user.avatar.url
   end
+
 end

a605f1cb8b75fb7387bed1ae50e657dac2f66708
songgz <sgzhe@163.com>
2020-04-12 16:29:47 +0800

    update to rials 6.0.0

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,11 +15,11 @@ Rails.application.configure do
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+  config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = true #ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
@@ -69,6 +69,9 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+  #logger = ActiveSupport::Logger.new("log/production.log")
+  #config.logger = ActiveSupport::TaggedLogging.new(logger)
+  #config.logger = ActiveSupport::Logger.new(config.paths['log'].first, shift_age = 'daily')
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)

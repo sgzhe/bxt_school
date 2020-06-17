4f447cc80858bd33b18b1f19dae95afb460437c3
songgz <sgzhe@163.com>
2020-06-17 21:03:42 +0800

    modify cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -33,7 +33,9 @@ class CardsController < ApplicationController
   # PATCH/PUT /cards/1
   # PATCH/PUT /cards/1.json
   def update
-    if @card.update(card_params)
+    ips = @card.card_access_ips.merge(card_params[:card_access_ips])
+
+    if @card.update(card_params.merge({card_access_ips: ips}))
       render :show, status: :ok, location: @card
     else
       render json: @card.errors, status: :unprocessable_entity

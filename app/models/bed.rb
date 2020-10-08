95e037b3173a7151b210e91c6304f359b725fad0
songgz <sgzhe@163.com>
2020-10-08 13:43:59 +0800

    modify field Sym and card face

--- app/models/bed.rb
+++ app/models/bed.rb
@@ -4,12 +4,14 @@ class Bed
   field :mark, type: String
   field :desc, type: String, default: ''
   field :owner_name, type: String, default: ''
+  field :owner_sno, type: String, default: ''
 
   belongs_to :owner, class_name: 'User', required: false
   embedded_in :room, class_name: 'Room'
 
   set_callback(:save, :before) do |doc|
-    doc.owner_name =  doc.owner.name if doc.owner
+    doc.owner_name =  doc.owner.try(:name)
+    doc.owner_sno = doc.owner.try(:sno)
   end
 
 end

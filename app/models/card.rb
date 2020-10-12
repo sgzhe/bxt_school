e5acbcdafce7290aa725faf54f753499f33ab7aa
songgz <sgzhe@163.com>
2020-10-13 00:09:56 +0800

    modify log for card and face

--- app/models/card.rb
+++ app/models/card.rb
@@ -19,12 +19,12 @@ class Card
 
   set_callback(:save, :before) do |doc|
     if doc.card_access_ips_changed?
-      if doc.status == :add
-        doc.status = :added if doc.card_access_ips.any? { |k, v| v == 1 }
+      if doc.status == 'add'
+        doc.status = 'added' if doc.card_access_ips.any? { |k, v| v == 1 }
       end
 
-      if doc.status == :delete
-        doc.status = :deleted if doc.card_access_ips.any? { |k, v| v == -1 }
+      if doc.status == 'delete'
+        doc.status = 'deleted' if doc.card_access_ips.any? { |k, v| v == -1 }
       end
 
       # if doc.status == :add || doc.status = :deleted

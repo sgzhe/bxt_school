560b56a9b3e52d25bdd542909e5dccac0f128567
songgz <sgzhe@163.com>
2020-10-26 22:32:20 +0800

    modify jwt

--- config/initializers/jwt_session.rb
+++ config/initializers/jwt_session.rb
@@ -4,7 +4,7 @@ JWTSessions.encryption_key = "secret"
 #JWTSessions.token_store = :memory
 if Rails.env == 'production'
  JWTSessions.token_store = :redis, {
-     redis_host: "10.200.2.249",
+     redis_host: ":bxt2020@10.200.2.249",
      redis_port: "6379",
      redis_db_name: "0",
      token_prefix: "jwt_"
